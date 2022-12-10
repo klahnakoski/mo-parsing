26bb54ed46f1ba1cb1763e0ac6f2d6f2a62c4ae2
Kyle Lahnakoski <kyle@lahnakoski.com>
2022-12-10 10:19:25 -0500

    fix smoke test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,15 +1,9 @@
 # encoding: utf-8
 from time import time
 
-from mo_dots import NullType
-
-from mo_parsing import Log
-
 start = time()
 from mo_parsing import *
-
 end = time()
-
 print(f"import time = {str(round(end-start, 2))} seconds")
 
 from mo_parsing.whitespaces import Whitespace
@@ -21,8 +15,3 @@ with Whitespace():
 integer = Word("0123456789") / (lambda t: int(t[0]))
 result = integer.parse_string("42")
 assert result[0] == 42
-
-try:
-    Log.error("this is a problem")
-except ParseException as cause:
-    assert isinstance(cause.expr, NullType)
